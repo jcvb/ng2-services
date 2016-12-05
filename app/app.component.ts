1fec005bb2e3412c19b24ebf051671611510ffec
Juan Carlos Valerio Barreto <jc.valerio.b@gmail.com>
2016-12-05 11:19:35 -0600

    primer servicio con ng2

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { ExampleService } from './example.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `{{ title }} <h1>Hello {{name}}</h1>`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	name = 'Angular'; 
+
+	title: string;
+
+	constructor( private _exampleService: ExampleService ) {
+		this.title = _exampleService.someMethod();
+	}
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ExampleService } from './example.service';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers:    [ExampleService]
 })
 export class AppModule { }
